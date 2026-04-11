954cadced2a8027f8c81430a4080defd554ffba7
Wendell Eduardo Albuquerque Bonucci <contact.wendellbonucci@gmail.com>
2026-04-11 19:46:35 -0300

    Importação da função de enviar Mensagem e Loanding para página de Contato

--- src/components/contact/ContactMain.tsx
+++ src/components/contact/ContactMain.tsx
@@ -1,11 +1,18 @@
 import InitialContact from "./InitialContact"
 import FormContact from "./FormContact"
+import Loading from "@/utils/Loanding/Loanding";
+import Messenger from "@/utils/Loanding/Messeger"
+
 
 export default function ContactMain() {
   return (
-    <section className="w-full min-h-screen bg-[#050505] flex flex-col items-center justify-center">
-      <InitialContact />
-      <FormContact />
-    </section>
+    <>
+      <Loading />
+      <section className="w-full min-h-screen bg-[#050505] flex flex-col items-center justify-center">
+        <InitialContact />
+        <FormContact />
+        <Messenger />
+      </section>
+    </>
   )
 }
