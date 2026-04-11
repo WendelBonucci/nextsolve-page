344a8fad447b09a6a61cd7770f53035b8968ff97
Wendell Eduardo Albuquerque Bonucci <contact.wendellbonucci@gmail.com>
2026-04-10 23:36:05 -0300

    Ajuste de Código

--- src/components/contact/ContactMain.tsx
+++ src/components/contact/ContactMain.tsx
@@ -3,7 +3,7 @@
 export default function ContactMain() {
   return (
     <section className="w-full min-h-screen bg-[#050505] flex items-center justify-center">
-      <h1 className="font-syne font-black text-4xl md:text-4xl lg:text-6xl text-white tracking-tight leading-tight">Página em Desenvolvimento!</h1>
+      <h1 className="text-center font-syne font-black text-4xl md:text-4xl lg:text-6xl text-white tracking-tight leading-tight">Página em Desenvolvimento!</h1>
     </section>
   )
 }
