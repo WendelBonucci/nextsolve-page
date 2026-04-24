eb5b953c3be3ac333904f5f010630b329966aab7
Wendell Eduardo Albuquerque Bonucci <contact.wendellbonucci@gmail.com>
2026-04-23 23:12:55 -0300

    Importação de Sessão

--- src/components/Service/Service.tsx
+++ src/components/Service/Service.tsx
@@ -1,11 +1,13 @@
 import ContentServiceInitial from "./sections/ContentServiceInitial"
 import CardsService from "./sections/CardsService"
+import FAQSession from "../FAQ/FAQ"
 
 export default function Service() {
     return (
         <main className='w-full h-auto bg-black'>
             <ContentServiceInitial />
             <CardsService />
+            <FAQSession />
         </main>
     )
 }
